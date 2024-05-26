f3e6cb8d2376ef7c1da8852cd711d8f42864a0ef
WilmarAgudelo <wagudelo352@gmail.com>
2024-05-26 09:55:08 -0500

    init

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import vue from '@vite/plugin-vue';
+
+export default defineConfig({
+    plugin: [vue()],
+    base: 'https://WilmarAgudelo.github.io/proyecto_final_web'
+
+
+
+})
